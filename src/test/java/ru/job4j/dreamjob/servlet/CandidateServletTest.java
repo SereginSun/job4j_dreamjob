1c86446d7993e75051b7b3e3a7730187d45953e4
SereginSun <SereginSun@yandex.ru>
2021-11-30 20:00:39 +0300

    changed mock tests for servlets.

--- src/test/java/ru/job4j/dreamjob/servlet/CandidateServletTest.java
+++ src/test/java/ru/job4j/dreamjob/servlet/CandidateServletTest.java
@@ -1,17 +1,10 @@
 package ru.job4j.dreamjob.servlet;
 
-import org.junit.Ignore;
 import org.junit.Test;
-import org.junit.runner.RunWith;
-import org.powermock.api.mockito.PowerMockito;
-import org.powermock.core.classloader.annotations.PrepareForTest;
-import org.powermock.modules.junit4.PowerMockRunner;
 import ru.job4j.dreamjob.models.Candidate;
-import ru.job4j.dreamjob.store.MemStore;
 import ru.job4j.dreamjob.store.PsqlStore;
 import ru.job4j.dreamjob.store.Store;
 
-import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
@@ -19,50 +12,39 @@ import java.io.IOException;
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.core.Is.is;
 import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
 
-@RunWith(PowerMockRunner.class)
-@PrepareForTest(PsqlStore.class)
 public class CandidateServletTest {
 
-    @Ignore
     @Test
-    public void whenCreateCandidate() throws IOException, ServletException {
-        Store store = MemStore.instOf();
-
-        PowerMockito.mockStatic(PsqlStore.class);
-        PowerMockito.when(PsqlStore.instOf()).thenReturn(store);
-
+    public void whenCreateCandidate() throws IOException {
+        Store store = PsqlStore.instOf();
+        store.clearTable("candidate");
         HttpServletRequest req = mock(HttpServletRequest.class);
         HttpServletResponse resp = mock(HttpServletResponse.class);
-
-        PowerMockito.when(req.getParameter("id")).thenReturn("0");
-        PowerMockito.when(req.getParameter("name")).thenReturn("n");
-
+        when(req.getParameter("id")).thenReturn("0");
+        when(req.getParameter("name")).thenReturn("Boris");
+        when(req.getParameter("city")).thenReturn("1");
         new CandidateServlet().doPost(req, resp);
-
-        Candidate result = store.findAllCandidates().iterator().next();
-        assertThat(result.getId(), is(5));
-        assertThat(result.getName(), is("n"));
+        Candidate result = store.findCandidateById(1);
+        assertThat(result.getName(), is("Boris"));
     }
 
-    @Ignore
     @Test
-    public void whenEditCandidate() throws IOException, ServletException {
-        Store store = MemStore.instOf();
-
-        PowerMockito.mockStatic(PsqlStore.class);
-        PowerMockito.when(PsqlStore.instOf()).thenReturn(store);
-
+    public void whenEditCandidate() throws IOException {
+        Store store = PsqlStore.instOf();
+        store.clearTable("candidate");
         HttpServletRequest req = mock(HttpServletRequest.class);
         HttpServletResponse resp = mock(HttpServletResponse.class);
-
-        PowerMockito.when(req.getParameter("id")).thenReturn("5");
-        PowerMockito.when(req.getParameter("name")).thenReturn("new n");
-
+        when(req.getParameter("id")).thenReturn("0");
+        when(req.getParameter("name")).thenReturn("Boris");
+        when(req.getParameter("city")).thenReturn("1");
         new CandidateServlet().doPost(req, resp);
-
-        Candidate result = store.findAllCandidates().iterator().next();
-        assertThat(result.getId(), is(5));
-        assertThat(result.getName(), is("new n"));
+        when(req.getParameter("id")).thenReturn("1");
+        when(req.getParameter("name")).thenReturn("Roman");
+        when(req.getParameter("city")).thenReturn("2");
+        new CandidateServlet().doPost(req, resp);
+        Candidate candidate = store.findCandidateById(1);
+        assertThat(candidate.getName(), is("Roman"));
     }
 }
--- src/test/java/ru/job4j/dreamjob/servlet/PostServletTest.java
+++ src/test/java/ru/job4j/dreamjob/servlet/PostServletTest.java
@@ -1,13 +1,7 @@
 package ru.job4j.dreamjob.servlet;
 
-import org.junit.Ignore;
 import org.junit.Test;
-import org.junit.runner.RunWith;
-import org.powermock.api.mockito.PowerMockito;
-import org.powermock.core.classloader.annotations.PrepareForTest;
-import org.powermock.modules.junit4.PowerMockRunner;
 import ru.job4j.dreamjob.models.Post;
-import ru.job4j.dreamjob.store.MemStore;
 import ru.job4j.dreamjob.store.PsqlStore;
 import ru.job4j.dreamjob.store.Store;
 
@@ -19,50 +13,39 @@ import java.io.IOException;
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.core.Is.is;
 import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
 
-@RunWith(PowerMockRunner.class)
-@PrepareForTest(PsqlStore.class)
 public class PostServletTest {
 
-    @Ignore
     @Test
     public void whenCreatePost() throws IOException, ServletException {
-        Store store = MemStore.instOf();
-
-        PowerMockito.mockStatic(PsqlStore.class);
-        PowerMockito.when(PsqlStore.instOf()).thenReturn(store);
-
+        Store store = PsqlStore.instOf();
+        store.clearTable("post");
         HttpServletRequest req = mock(HttpServletRequest.class);
         HttpServletResponse resp = mock(HttpServletResponse.class);
-
-        PowerMockito.when(req.getParameter("id")).thenReturn("0");
-        PowerMockito.when(req.getParameter("name")).thenReturn("n");
-
+        when(req.getParameter("id")).thenReturn("0");
+        when(req.getParameter("name")).thenReturn("Some vacancy");
+        when(req.getParameter("description")).thenReturn("Some description");
         new PostServlet().doPost(req, resp);
-
-        Post result = store.findAllPosts().iterator().next();
-        assertThat(result.getId(), is(5));
-        assertThat(result.getName(), is("n"));
+        Post result = store.findPostById(1);
+        assertThat(result.getName(), is("Some vacancy"));
     }
 
-    @Ignore
     @Test
     public void whenEditPost() throws IOException, ServletException {
-        Store store = MemStore.instOf();
-
-        PowerMockito.mockStatic(PsqlStore.class);
-        PowerMockito.when(PsqlStore.instOf()).thenReturn(store);
-
+        Store store = PsqlStore.instOf();
+        store.clearTable("post");
         HttpServletRequest req = mock(HttpServletRequest.class);
         HttpServletResponse resp = mock(HttpServletResponse.class);
-
-        PowerMockito.when(req.getParameter("id")).thenReturn("5");
-        PowerMockito.when(req.getParameter("name")).thenReturn("new n");
-
+        when(req.getParameter("id")).thenReturn("0");
+        when(req.getParameter("name")).thenReturn("Some vacancy");
+        when(req.getParameter("description")).thenReturn("Some description");
         new PostServlet().doPost(req, resp);
-
-        Post result = store.findAllPosts().iterator().next();
-        assertThat(result.getId(), is(5));
-        assertThat(result.getName(), is("new n"));
+        when(req.getParameter("id")).thenReturn("1");
+        when(req.getParameter("name")).thenReturn("New vacancy");
+        when(req.getParameter("description")).thenReturn("New description");
+        new PostServlet().doPost(req, resp);
+        Post result = store.findPostById(1);
+        assertThat(result.getName(), is("New vacancy"));
     }
 }
